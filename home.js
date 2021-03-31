53a97793eac5f7e739ddd76c1ce8381b60915e37
Katerina_DI <dav99745@gmail.com>
2021-03-31 12:02:15 +0300

    one home work

---
+++ home.js
@@ -0,0 +1,57 @@
+let str='Привет'
+    console.log (typeof str)
+let num=123
+    console.log (typeof num)
+let flag=true
+    console.log (typeof flag)
+let txt='true'
+    console.log (typeof txt)
+
+
+let a1=5+3
+console.log (a1)
+let a2=5-3
+console.log ( a2)
+let a3=5*3
+console.log ( a3)
+let a4=5/3
+console.log ( a4)
+
+let a6=5%3
+console.log ( a6)
+let a7=3%5
+console.log ( a7)
+let a8=5+'3'
+console.log ( a8)
+let a9=75+' кг'
+console.log ( a9)
+let a10='5'-3
+console.log ( a10)
+
+const h=23
+const w=10
+const s=h*w/2+' см'
+console.log (s)
+
+const hC=10
+const dC=4
+const v=Math.PI*((dC/2)**2)*hC
+console.log (v)
+
+const n=3
+const m=4
+const k2=Math.pow(n, 2)+Math.pow(m, 2)
+const k=Math.sqrt(k2)
+console.log (k)
+
+let nFib=12, el12
+let a=(1+Math.sqrt(5))/2
+let b=(1-Math.sqrt(5))/2
+let c=Math.round((Math.pow(a, nFib)-Math.pow(b, nFib))/Math.sqrt(5))
+console.log (c)
+
+let S=2000000
+let p=0.1
+let y=5
+let perepl=S*p*y
+console.log (perepl)
